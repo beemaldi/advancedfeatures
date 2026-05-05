701bcf5065f1596a86f1fd9d59f5246b93c2490e
beemaldi <berni.maucher@gmail.com>
2026-05-05 17:40:27 +0200

    Add files via upload

---
+++ source NoRepair/NoRepairClothesBehavior.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace RuckSack
+{
+    public class NoRepairClothesBehavior : CollectibleBehavior
+    {
+        public NoRepairClothesBehavior(CollectibleObject collObj) : base(collObj)
+        {
+        }
+    }
+}
